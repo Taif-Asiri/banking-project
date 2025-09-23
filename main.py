2ae0d75f22ece0f3d33b1c1a0ae1aa184e6f4077
Taif-Asiri <toti4438@gmail.com>
2025-09-24 01:28:22 +0300

    Start writing main.py file

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ def main():
     print("=== Welcome to ACME Bank ===")
     account_id = input("Enter your account ID: ")
     password = input("Enter your password: ")
+    
+if __name__ == "__main__":
+    main()
